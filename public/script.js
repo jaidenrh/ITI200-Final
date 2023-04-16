c22cbc037296adcef1bb04081ffaecd0ff94c501
jaidenrh <99220246+jaidenrh@users.noreply.github.com>
2023-04-16 17:51:56 -0400

    pg added + minor database additions
    Gibby doesn't know what to put here

--- public/script.js
+++ public/script.js
@@ -1,21 +1,68 @@
-// Get the username from localStorage
 var savedUsername = localStorage.getItem('username');
+      console.log(savedUsername);
+      var defaultMoney = 500;
 
-// If a username is found, redirect to wheel.html with the saved username
-if (savedUsername) {
-  window.location.href = '/roulette?username=' + encodeURIComponent(savedUsername);
-}
+      $(document).ready(function() {
+        // If a username is found, redirect to wheel.html with the saved username
+        if (savedUsername) {
+          console.log("Found saved Username")
+          // check if the saved username exists in the database
+          var xhttp = new XMLHttpRequest();
+          xhttp.open("POST", "/check-user");
+          xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+          xhttp.onload = function() {
+            console.log(this.responseText);
+            var userExists = JSON.parse(this.responseText);
+            if (userExists == true) {
+              console.log("User Exists");
+              window.location.href = '/roulette?username=' + encodeURIComponent(savedUsername);
+            } 
+            if (userExists == false) {
+              // if the user doesn't exist, save the username and default money value to the database
+              saveUser(savedUsername, defaultMoney);
+            }
 
-// If no saved username is found, display the login form
-else {
-  // code for displaying the login form
-}
+          };
+          xhttp.send("username=" + encodeURIComponent(savedUsername));
+        }
 
-// Save the username to localStorage when the user submits the form
-var form = document.getElementById('login-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // prevent form submission
-  var username = document.getElementById('username').value;
-  localStorage.setItem('username', username);
-  window.location.href = '/roulette?username=' + encodeURIComponent(username);
-});
+
+      // If no saved username is found, display the login form
+      else {
+        // code for displaying the login form
+      }
+
+      });
+
+      // Save the username and default money value to localStorage and the database when the user submits the form
+      var form = document.getElementById('login-form');
+      form.addEventListener('submit', function(event) {
+        event.preventDefault(); // prevent form submission
+        var username = document.getElementById('username').value;
+        localStorage.setItem('username', username);
+        localStorage.setItem('money', defaultMoney);
+        // check if the entered username exists in the database
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/check-user");
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.onload = function() {
+          var userExists = JSON.parse(this.responseText);
+          if (userExists) {
+            window.location.href = '/roulette?username=' + encodeURIComponent(username);
+          } else {
+            // if the user doesn't exist, save the username and default money value to the database
+            saveUser(username, defaultMoney);
+            window.location.href = '/roulette?username=' + encodeURIComponent(username);
+          }
+        };
+        xhttp.send("username=" + encodeURIComponent(username));
+      });
+
+      function saveUser(username, money) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "/save-user");
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhttp.send("username=" + encodeURIComponent(username) + "&money=" + encodeURIComponent(money));
+        console.log("Saving User...");
+        window.location.href = '/roulette?username=' + encodeURIComponent(savedUsername);
+      }
--- server.js
+++ server.js
@@ -1,20 +1,66 @@
 const express = require('express');
 const path = require('path');
 const WebSocket = require('ws');
+const bodyParser = require('body-parser');
 const app = express();
+const Pool = require('pg').Pool;
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+const pool = new Pool({
+    host: 'localhost',
+    database: 'postgres',
+    user: 'postgres',
+    password: 'Homk0487!',
+    port: 5432,
+});
 
 app.get('/main', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'main.html'));
-  });
+});
+
 app.get('/roulette', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'wheel.html'));
-  });
+});
+
 app.get('/admin', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'admin.html'));
-  });
+});
+
+// Handle POST request to save user information to the database
+app.post('/save-user', (req, res) => {
+    const {username, money} = req.body;
+    pool.query('INSERT INTO UserMoney (username, money) VALUES ($1, $2)', [username, money], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Error saving user information to the database');
+        } else {
+            res.status(200).send('User information saved to the database');
+        }
+    });
+});
+
+app.post('/check-user', (req, res) => {
+    console.log("checking user...");
+    const {username} = req.body;
+    pool.query('SELECT * FROM UserMoney WHERE username = $1', [username], (error, results) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send({error: 'Error checking user information in the database'});
+        } else {
+            if (results.rows.length === 0) {
+                console.log("False");
+                res.status(200).send(false);
+            } else {
+                console.log("True");
+                res.status(200).send(true);
+            }
+        }
+    });
+});
+
 
 const wss = new WebSocket.Server({ port: 8080 });
 
